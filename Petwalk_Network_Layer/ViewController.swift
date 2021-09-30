7d778c9938c52df9ae358df10de2882080a56506
Alina-Petrovskaya <alina.petrovskaia92@gmail.com>
2021-09-30 14:15:33 +0300

    Initial Commit

---
+++ Petwalk_Network_Layer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Petwalk_Network_Layer
+//
+//  Created by Alina Petrovskaya on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
